162633b50ce6429501357fd3c59b87798364a987
tyh2333 <tyh_yk@163.com>
2021-04-19 09:52:59 -0700

    Update simple2.py

--- examples/simple2.py
+++ examples/simple2.py
@@ -15,23 +15,37 @@
 ########################################################################
 
 import time
+import sys
 import etw
 
+# append the path of pywintrace-master
+sys.path.append('''C:\\Users\\tyh\\desktop\\pywintrace-master''')
+
+
+# python -u "C:\Users\tyh_2\desktop\pywintrace-master\examples\simple2.py"
 
 def some_func():
     # define capture provider info
-    providers = [etw.ProviderInfo('Some Provider', etw.GUID("{11111111-1111-1111-1111-111111111111}"))]
-    # create instance of ETW class
-    job = etw.ETW(providers=providers, event_callback=lambda x: print(x))
-    # start capture
-    job.start()
 
-    # wait some time
-    time.sleep(5)
+    providers0 = [etw.ProviderInfo('PowerShell', etw.GUID("{A0C1853B-5C40-4B15-8766-3CF1C58F985A}"))]
+    providers1 = [etw.ProviderInfo('Kernel-Registry', etw.GUID("{70EB4F03-C1DE-4F73-A051-33D13D5413BD}"))]
+    providers2 = [etw.ProviderInfo('Kernel-Network', etw.GUID("{7DD42A49-5329-4832-8DFD-43D979153A88}"))]
+    providers3 = [etw.ProviderInfo('Kernel-File', etw.GUID("{EDD08927-9CC4-4E65-B970-C2560FB5C289}"))]
+    providers4 = [etw.ProviderInfo('Kernel-Audit-API-Calls', etw.GUID("{E02A841C-75A3-4FA7-AFC8-AE09CF9B7F23}"))]
+    providers5 = [etw.ProviderInfo('Kernel-Disk', etw.GUID("{C7BDE69A-E1E0-4177-B6EF-283AD1525271}"))]
+    providers6 = [etw.ProviderInfo('HttpEvent', etw.GUID("{7B6BC78C-898B-4170-BBF8-1A469EA43FC5}"))]
+    providers7 = [etw.ProviderInfo('HttpLog', etw.GUID("{C42A2738-2333-40A5-A32F-6ACC36449DCC}"))]
+    providers8 = [etw.ProviderInfo('HttpService', etw.GUID("{DD5EF90A-6398-47A4-AD34-4DCECDEF795F}"))]
 
-    # stop capture
-    job.stop()
+    # change providers0 to providers1,2,..8 to get other logs
+    job0 = etw.ETW(providers=providers3, event_callback=lambda x: print(x))
+    job0.start()
+    print('start')
+    time.sleep(100)
+    print('end')
+    job0.stop()
 
 
 if __name__ == '__main__':
     some_func()
+
